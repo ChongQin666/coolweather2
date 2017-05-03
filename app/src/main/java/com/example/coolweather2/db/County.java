76115ce76e3aec10d343d386b3c45218e4c9b8b3
Chong <1055059962@qq.com>
2017-05-03 23:54:16 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/coolweather2/db/County.java
@@ -0,0 +1,45 @@
+package com.example.coolweather2.db;
+
+/**
+ * Created by Administrator on 2017/5/3.
+ */
+
+public class County
+{
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private  int cityId;
+}
